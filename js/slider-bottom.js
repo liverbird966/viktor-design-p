d3e6869073a79602ec2bcc4b88757e03daaeae4a
liverbird966 <60104830+liverbird966@users.noreply.github.com>
2020-05-18 14:01:26 +0300

    ...

---
+++ js/slider-bottom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      infinite: true,
+      speed: 600,
+      slidesToShow: 2,
+      autoplay: true,
+      autoplaySpeed: 5000
+    });
+    
+});
