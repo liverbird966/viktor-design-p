314681a9e989c72e2fd36a447dd1e83c18d49bca
liverbird966 <60104830+liverbird966@users.noreply.github.com>
2020-05-24 12:16:22 +0300

    ..

--- js/color-change.js
+++ js/color-change.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
     $("button").on("click", "#white", function() {
-      $('.box-inside, .select-css, .select-css-1').css("background-color", "#ffffff");
+      $('.follow-me-box, .follow-me-box i').css("color", "#ffffff");
     });
   
     $("button").on("click", "#darkturquoise", function() {
-      $('.box-inside, .select-css, .select-css-1').css("background-color", "#00CED1");
+      $('.follow-me-box, .follow-me-box i').css("color", "#00CED1");
     });
   
     $("button").on("click", "#brown", function() {
-      $('.box-inside, .select-css, .select-css-1').css("background-color", "#A52A2A");
+      $('.follow-me-box, .follow-me-box i').css("color", "#A52A2A");
     });
   
     $("button").on("click", "#limegreen", function() {
-      $('.box-inside, .select-css, .select-css-1').css("background-color", "#32CD32");
+      $('.follow-me-box, .follow-me-box i').css("color", "#32CD32");
     });
   
     $("button").on("click", "#royalblue", function() {
-      $('.box-inside, .select-css, .select-css-1').css("background-color", "#4169E1");
+      $('.follow-me-box, .follow-me-box i').css("color", "#4169E1");
     });
   
     $("button").on("click", "#goldenrod", function() {
-      $('.box-inside, .select-css, .select-css-1').css("background-color", "#DAA520");
+      $('.follow-me-box, .follow-me-box i').css("color", "#DAA520");
     });
   
     $("button").on("click", "#tomato", function() {
-      $('.box-inside, .select-css, .select-css-1').css("background-color", "#FF6347");
+      $('.follow-me-box, .follow-me-box i').css("color", "#FF6347");
     });
   
     $("button").on("click", "#cornsilk", function() {
-      $('.box-inside, .select-css, .select-css-1').css("background-color", "#FFF8DC");
+      $('.follow-me-box, .follow-me-box i').css("color", "#FFF8DC");
     });
 
     $(".moving-colors button").on('click', function() {
